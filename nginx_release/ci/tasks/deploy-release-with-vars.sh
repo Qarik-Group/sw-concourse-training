26c7c0461a5e56f3ac5c007c7c16dfc6e796b60b
David Dobmeier <daviddob@buffalo.edu>
2018-10-04 21:31:29 -0400

    WIP Vars

--- nginx_release/ci/tasks/deploy-release-with-vars.sh
+++ nginx_release/ci/tasks/deploy-release-with-vars.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 cd source-code/nginx_release
-
-curl -LO https://unreal-snw.s3.amazonaws.com/training-bosh.pem
-bosh alias-env --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR} training-bosh
-bosh login
-
 bosh deploy --non-interactive manifests/manifest.yml
--- nginx_release/ci/tasks/upload-release-with-vars.sh
+++ nginx_release/ci/tasks/upload-release-with-vars.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
-set -x
-#For the purpose of this tutorial, there are credentials being commited here.
-#This is on purpose and will be covered in the security tutorial.
-#The director is expected to be secured and only locally available for this lab session
-#But this does not demonostrate a best practice
-
 cd source-code/nginx_release
-
-curl -LO https://unreal-snw.s3.amazonaws.com/training-bosh.pem
-bosh alias-env --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR} training-bosh
-
-bosh login
-
 bosh upload-release releases/release.gz
