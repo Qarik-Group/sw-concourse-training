294cc891de3c14236cbdba3bd86fce0993edb033
Norman Abramovitz <norman-abramovitz@users.noreply.github.com>
2018-10-31 14:40:13 -0700

    replace dave's name and secret with replace-me

--- nginx_release/ci/tasks/upload-release.sh
+++ nginx_release/ci/tasks/upload-release.sh
@@ -5,10 +5,10 @@ set -x
 #The director is expected to be secured and only locally available for this lab session
 #But this does not demonostrate a best practice
 
-export BOSH_DEPLOYMENT=dave-nginx
+export BOSH_DEPLOYMENT=<replace-me>-nginx
 export BOSH_DIRECTOR=https://10.200.192.0:25555/
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=agile-defense
+export BOSH_CLIENT_SECRET=<replace-me>
 export BOSH_ENVIRONMENT=training-bosh
 
 cd source-code/nginx_release
