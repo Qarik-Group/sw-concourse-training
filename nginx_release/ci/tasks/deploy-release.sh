dda5b1a48bca8a084e5f96221e7bf0834035e1ad
Tyler Poland <tpoland@starkandwayne.com>
2018-11-08 11:08:00 -0500

    Update deploy-release.sh

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export CA_CERT_URL=https://unreal-snw.s3.amazonaws.com/training-bosh.pem
 export BOSH_DEPLOYMENT=<replace-me>-nginx
 export BOSH_DIRECTOR=https://10.200.192.0:25555/
 export BOSH_CLIENT=admin
@@ -8,8 +9,8 @@ export BOSH_ENVIRONMENT=training-bosh
 
 cd source-code/nginx_release
 
-curl -LO https://unreal-snw.s3.amazonaws.com/training-bosh.pem
-bosh alias-env --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR} training-bosh
+curl -LO ${CA_CERT_URL}
+bosh alias-env ${BOSH_ENVIRONMENT} --ca-cert training-bosh.pem -e ${BOSH_DIRECTOR}
 bosh login
 
 bosh deploy --non-interactive manifests/manifest.yml
